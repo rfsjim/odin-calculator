cfa17aa0294609395a80d455bfb1239d02eb20a7
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-22 21:23:36 +1100

    feat: adds maths functions for common caluclations

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 /**
  * Functions for all of the basic math operators you typically find on calculators
- *  addition
+ * addition
  * subtraction
  * multiplication
  * division
  */
+
+function addition(a, b)
+{
+    return a + b;
+}
+
+function subtraction(a, b)
+{
+    return a - b;
+}
+
+function multiplication(a, b)
+{
+    return a * b;
+}
+
+function division(a, b)
+{
+    return a / b;
+}
+
+function power(base, exponent)
+{
+    return base ** exponent;
+}
