350535a7abe29322528527279090db90416d47f7
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-23 19:47:40 +1100

    feat: Helper functions, add, subtract, divide, multiply, powers, function to operate

--- js/script.js
+++ js/script.js
@@ -6,27 +6,90 @@
  * division
  */
 
+/**
+ * Add two numbers together
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} 
+ */
 function addition(a, b)
 {
     return a + b;
 }
 
+/**
+ * Subtracts two numbers together 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
 function subtraction(a, b)
 {
     return a - b;
 }
 
+/**
+ * Multiplies two numbers together
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
 function multiplication(a, b)
 {
     return a * b;
 }
 
+/**
+ * Divides two numbers
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
 function division(a, b)
 {
     return a / b;
 }
 
+/**
+ * Calculates power
+ * @param {number} base 
+ * @param {number} exponent 
+ * @returns {number}
+ */
 function power(base, exponent)
 {
     return base ** exponent;
+}
+
+// A calculator operation will consist of a number, an operator, and another number.
+let numberA, numberB, operator;
+
+/**
+ * Take an operator and two numbers, then call a mathematically function
+ * @param {string} operator 
+ * @param {number} numberA 
+ * @param {number} numberB
+ * @returns {number} 
+ */
+function operate(operator, numberA, numberB)
+{
+    const availableOperators = ['+', '-', '*', '/', '**'];
+
+    if (!availableOperators.some(element => element === operator)) return 0;
+    if (Number.isNaN(numberA) || Number.isNaN(numberB)) return 0;
+
+    switch (availableOperators) {
+        case '+':
+            return addition(numberA, numberB);
+        case '-':
+            return subtraction(numberA, numberB);
+        case '*':
+            return multiplication(numberA, numberB);
+        case '/':
+            return division(numberA, numberB);
+        case '**':
+            return power(numberA, numberB);
+        default:
+            return 0;
+    }
 }
